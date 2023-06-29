385bb09e652c7209471661e7e1595e31d87b4da4
erasels <emanuelbedburdick@yahoo.de>
2023-06-29 21:01:59 +0200

    Move ColorlessCreations back

--- src/main/java/thePackmaster/patches/_expansionpacks/BattleStartHookPatch.java
+++ src/main/java/thePackmaster/patches/_expansionpacks/BattleStartHookPatch.java
@@ -3,7 +3,6 @@ package thePackmaster.patches._expansionpacks;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import thePackmaster.cards.colorlesspack.GolfBall;
 import thePackmaster.patches.secretlevelpack.EnoughTalkPatch;
 import thePackmaster.patches.secretlevelpack.SpecialCardGlowCheckPatch;
 
@@ -13,6 +12,5 @@ public class BattleStartHookPatch {
     public static void patch(AbstractPlayer __instance) {
         EnoughTalkPatch.spokeLastTurn = false;
         SpecialCardGlowCheckPatch.playedGlowingCardThisTurn = false;
-        GolfBall.BLOCK_AMT_LOST = 0;
     }
 }
