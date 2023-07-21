6d0a5b5a47d94c41565b323956655bd655c719ff
erasels <emanuelbedburdick@yahoo.de>
2023-07-21 13:19:20 +0200

    Rework MealPrep

--- src/main/java/thePackmaster/cards/sneckopack/Gulp.java
+++ src/main/java/thePackmaster/cards/sneckopack/Gulp.java
@@ -1,29 +1,59 @@
 package thePackmaster.cards.sneckopack;
 
+import basemod.helpers.TooltipInfo;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 import thePackmaster.cards.AbstractPackmasterCard;
+import thePackmaster.powers.TempHPRegenPower;
 import thePackmaster.util.Wiz;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
 public class Gulp extends AbstractSneckoCard {
 
     public final static String ID = makeID("Gulp");
-    private static final int DMG = 17, UPG_DMG = 5;
+    private static final int ABL = 3, UPG_ABL = 1;
+
+    private static TooltipInfo ablation;
 
     public Gulp() {
-        super(ID, 1, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
-        baseDamage = DMG;
+        super(ID, 1, CardType.ATTACK, CardRarity.RARE, CardTarget.SELF);
+        baseMagicNumber = magicNumber = ABL;
+        exhaust = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        Wiz.atb(new SFXAction("EVENT_VAMP_BITE", 0.05F));
         Wiz.atb(new AbstractGameAction() {
             @Override
             public void update() {
-                for (int i = 0; i < Wiz.getLogicalCardCost(Gulp.this); i++) {
-                    Wiz.doDmg(m, Gulp.this, AttackEffect.POISON, true);
+                int energySpent = Wiz.getLogicalCardCost(Gulp.this);
+
+                if (energySpent > 0) {
+                    //Triggers after gain
+                    Wiz.att(new AbstractGameAction() {
+                        @Override
+                        public void update() {
+                            isDone = true;
+                            AbstractPower pow = p.getPower(TempHPRegenPower.POWER_ID);
+                            if (pow != null) {
+                                //Trigger power energySpent times
+                                for (int i = 0; i < energySpent; i++) {
+                                    pow.atEndOfTurn(true);
+                                }
+                            }
+                        }
+                    });
+
+                    //Happens first
+                    Wiz.applyToSelfTop(new TempHPRegenPower(p, energySpent * magicNumber));
                 }
                 isDone = true;
             }
@@ -31,6 +61,15 @@ public class Gulp extends AbstractSneckoCard {
     }
 
     public void upp() {
-        upgradeDamage(UPG_DMG);
+        upgradeMagicNumber(UPG_ABL);
+    }
+
+    public List<TooltipInfo> getCustomTooltips() {
+        if (ablation == null) {
+            AbstractPower p = new TempHPRegenPower(null, 999);
+            ablation = new TooltipInfo(p.name, p.description.replace("999", "X"));
+        }
+
+        return Arrays.asList(ablation);
     }
 }
