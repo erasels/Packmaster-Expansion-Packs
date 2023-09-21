6e04904d060f51554ce26e7a09d1691391bc9fcb
modargo <modargo@gmail.com>
2023-09-21 05:39:48 -0400

    Pick Them All pack: add Grab and Go

---
+++ src/main/java/thePackmaster/cards/pickthemallpack/GrabAndGo.java
@@ -0,0 +1,43 @@
+package thePackmaster.cards.pickthemallpack;
+
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.OnObtainCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.relics.QuestionCard;
+import thePackmaster.SpireAnniversary5Mod;
+
+public class GrabAndGo extends AbstractPickThemAllCard implements OnObtainCard {
+    public static final String ID = SpireAnniversary5Mod.makeID("GrabAndGo");
+    private static final int COST = -2;
+
+    public GrabAndGo() {
+        super(ID, COST, CardType.SKILL, CardRarity.RARE, CardTarget.NONE);
+        this.isInnate = true;
+    }
+
+    @Override
+    public void upp() {
+        this.isInnate = false;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+    }
+
+    @Override
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        return false;
+    }
+
+    @Override
+    public void onObtainCard() {
+        AbstractDungeon.getCurrRoom().spawnRelicAndObtain((float) Settings.WIDTH / 2.0F, (float) Settings.HEIGHT / 2.0F, new QuestionCard());
+    }
+
+    @Override
+    public void onRemoveFromMasterDeck() {
+        AbstractDungeon.player.loseRelic(QuestionCard.ID);
+    }
+}
