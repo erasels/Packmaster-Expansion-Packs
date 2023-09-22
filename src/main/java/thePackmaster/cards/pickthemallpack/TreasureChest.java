ce68a1bf579035a6b22c1a14f9f7ab9852f8a6f4
modargo <modargo@gmail.com>
2023-09-22 08:36:17 -0400

    Pick Them All pack: fix Treasure Chest not decrementing the master deck version

--- src/main/java/thePackmaster/cards/pickthemallpack/TreasureChest.java
+++ src/main/java/thePackmaster/cards/pickthemallpack/TreasureChest.java
@@ -1,7 +1,9 @@
 package thePackmaster.cards.pickthemallpack;
 
 import basemod.abstracts.CustomSavable;
+import com.evacipated.cardcrawl.mod.stslib.StSLib;
 import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.OnObtainCard;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -34,6 +36,11 @@ public class TreasureChest extends AbstractPickThemAllCard implements OnObtainCa
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.decrementMisc();
         this.initializeDescription();
+        TreasureChest masterDeckCard = (TreasureChest)StSLib.getMasterDeckEquivalent(this);
+        if (masterDeckCard != null) {
+            masterDeckCard.decrementMisc();
+            masterDeckCard.initializeDescription();
+        }
     }
 
     @Override
