7016d52d66753c424ba9afc3d4f70b058ede55eb
modargo <modargo@gmail.com>
2023-11-12 20:03:26 -0500

    Magnetize pack: fix polarity behavior when you have 0 polarity and gain 10+ and when you gain enough at once to gain 2+ energy

--- src/main/java/thePackmaster/powers/magnetizepack/PolarityPower.java
+++ src/main/java/thePackmaster/powers/magnetizepack/PolarityPower.java
@@ -15,15 +15,27 @@ public class PolarityPower extends AbstractPackmasterPower implements CloneableP
     public static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
     public static final String[] DESCRIPTIONS = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
 
+    public static final int STACKS_PER_ENERGY = 10;
+
     public PolarityPower(AbstractCreature owner, int amount) {
         super(POWER_ID, NAME, PowerType.BUFF, false, owner, amount);
     }
 
     public void stackPower(int stackAmount) {
         super.stackPower(stackAmount);
-        if (amount >= 10) {
-            addToTop(new GainEnergyAction(1));
-            amount -= 10;
+        this.convertPolarityToEnergy();
+    }
+
+    @Override
+    public void onInitialApplication() {
+        this.convertPolarityToEnergy();
+    }
+
+    private void convertPolarityToEnergy() {
+        if (amount >= STACKS_PER_ENERGY) {
+            int energy = amount / STACKS_PER_ENERGY;
+            addToTop(new GainEnergyAction(energy));
+            amount -= (energy * STACKS_PER_ENERGY);
             if (amount <= 0) {
                 addToTop(new RemoveSpecificPowerAction(owner, owner, this));
             }
