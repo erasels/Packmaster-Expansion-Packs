6f916366b951c96103351835e19589deecb6a610
DarkVexon <edanvirgo@gmail.com>
2023-12-02 11:35:53 -0700

    Glock-on card change

--- src/main/java/thePackmaster/cards/lockonpack/GlockOn.java
+++ src/main/java/thePackmaster/cards/lockonpack/GlockOn.java
@@ -1,13 +1,9 @@
 package thePackmaster.cards.lockonpack;
 
-import basemod.helpers.CardModifierManager;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.FocusPower;
 import com.megacrit.cardcrawl.powers.LockOnPower;
-import thePackmaster.cardmodifiers.lockonpack.GlockOnModifier;
-import thePackmaster.patches.hermitpack.EnumPatch;
+import thePackmaster.powers.lockonpack.GlockOnPower;
 import thePackmaster.util.Wiz;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
@@ -17,18 +13,18 @@ public class GlockOn extends AbstractLockonCard {
     public final static String ID = makeID(GlockOn.class.getSimpleName());
 
     public GlockOn() {
-        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = damage = 8;
-        CardModifierManager.addModifier(this, new GlockOnModifier());
+        super(ID, 0, CardType.SKILL, CardRarity.COMMON, CardTarget.ENEMY);
+        baseMagicNumber = magicNumber = 1;
     }
 
     @Override
     public void upp() {
-        upgradeDamage(3);
+        upgradeMagicNumber(1);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.doDmg(m, damage, damageTypeForTurn, EnumPatch.HERMIT_GUN);
+        Wiz.applyToEnemy(m, new LockOnPower(m, magicNumber));
+        Wiz.applyToSelf(new GlockOnPower(p, 1));
     }
 }
