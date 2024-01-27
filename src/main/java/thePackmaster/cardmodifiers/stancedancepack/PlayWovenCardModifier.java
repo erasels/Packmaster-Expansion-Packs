5c33cbe0e0ad12499c144ba9444a5ce3e5e46546
DarkVexon <edanvirgo@gmail.com>
2024-01-26 20:04:35 -0700

    Fixes

--- src/main/java/thePackmaster/cardmodifiers/stancedancepack/PlayWovenCardModifier.java
+++ src/main/java/thePackmaster/cardmodifiers/stancedancepack/PlayWovenCardModifier.java
@@ -1,8 +1,6 @@
 package thePackmaster.cardmodifiers.stancedancepack;
 
 import basemod.abstracts.AbstractCardModifier;
-import com.megacrit.cardcrawl.actions.common.ReduceCostAction;
-import com.megacrit.cardcrawl.actions.common.ReduceCostForTurnAction;
 import com.megacrit.cardcrawl.actions.utility.NewQueueCardAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -10,10 +8,7 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.LocalizedStrings;
 import com.megacrit.cardcrawl.localization.UIStrings;
-import thePackmaster.SpireAnniversary5Mod;
 import thePackmaster.util.Wiz;
-
-import java.util.Objects;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -22,15 +17,17 @@ public class PlayWovenCardModifier extends AbstractCardModifier {
     private static UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(ID);
 
     private AbstractCard cardToPlay;
+    private boolean isLast;
 
-    public PlayWovenCardModifier(AbstractCard cardIn) {
+    public PlayWovenCardModifier(AbstractCard cardIn, boolean isLast) {
         cardToPlay = cardIn;
         cardToPlay.purgeOnUse = true;
+        this.isLast = isLast;
     }
 
     @Override
     public String modifyDescription(String rawDescription, AbstractCard card) {
-            return rawDescription + uiStrings.TEXT[0] + cardToPlay.name + LocalizedStrings.PERIOD + " NL " ;
+        return rawDescription + uiStrings.TEXT[0] + cardToPlay.name + LocalizedStrings.PERIOD + (this.isLast ? "" : " NL ");
     }
 
     @Override
@@ -41,6 +38,6 @@ public class PlayWovenCardModifier extends AbstractCardModifier {
 
     @Override
     public AbstractCardModifier makeCopy() {
-        return new PlayWovenCardModifier(cardToPlay);
+        return new PlayWovenCardModifier(cardToPlay, this.isLast);
     }
 }
