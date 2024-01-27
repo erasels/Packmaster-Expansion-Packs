46393bf6d1c49c76502c84ee8e1f8fc430e949ec
mikemayhemdev <45570650+mikemayhemdev@users.noreply.github.com>
2024-01-26 22:57:29 -0500

    asdf

--- src/main/java/thePackmaster/cards/stancedancepack/StagLeap.java
+++ src/main/java/thePackmaster/cards/stancedancepack/StagLeap.java
@@ -2,6 +2,7 @@ package thePackmaster.cards.stancedancepack;
 
 
 import basemod.Pair;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
@@ -11,6 +12,7 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.GainStrengthPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.vfx.combat.ShockWaveEffect;
 
@@ -37,13 +39,17 @@ public class StagLeap extends AbstractStanceDanceCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new VFXAction(p, new ShockWaveEffect(p.hb.cX, p.hb.cY, Settings.GOLD_COLOR, ShockWaveEffect.ShockWaveType.NORMAL), 0.3F));
 
-        this.addToBot(new ChangeStanceAction(new CunningStance()));
         AbstractDungeon.player.useJumpAnimation();
         Wiz.atb(new VFXAction(new StanceDanceEffect(p, false, true, false), 0.5F));
 
         forAllMonstersLiving((mo)->{
-            Wiz.applyToEnemy(mo, new StrengthPower(mo, magicNumber));
+            Wiz.applyToEnemy(mo, new StrengthPower(mo, magicNumber*-1));
+            if (!mo.hasPower("Artifact")) {
+                this.addToBot(new ApplyPowerAction(mo, p, new GainStrengthPower(mo, this.magicNumber), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
+            }
         });
+
+        this.addToBot(new ChangeStanceAction(new CunningStance()));
     }
 
     @Override
