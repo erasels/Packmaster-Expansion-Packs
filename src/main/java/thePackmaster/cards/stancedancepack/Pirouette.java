46a146774f06a0fd21c3cdaf1d08094e96e235e5
mikemayhemdev <45570650+mikemayhemdev@users.noreply.github.com>
2024-01-28 19:49:58 -0500

    Update Pirouette.java

--- src/main/java/thePackmaster/cards/stancedancepack/Pirouette.java
+++ src/main/java/thePackmaster/cards/stancedancepack/Pirouette.java
@@ -3,12 +3,15 @@ package thePackmaster.cards.stancedancepack;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.actions.watcher.ChangeStanceAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
 import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.vfx.combat.CleaveEffect;
 import thePackmaster.actions.EasyXCostAction;
 
 import thePackmaster.stances.downfallpack.AncientStance;
@@ -26,6 +29,7 @@ public class Pirouette extends AbstractStanceDanceCard {
     public Pirouette() {
         super(ID, -1, CardType.ATTACK, CardRarity.RARE, CardTarget.ALL_ENEMY);
         baseDamage = 6;
+        this.isMultiDamage = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
@@ -34,7 +38,9 @@ public class Pirouette extends AbstractStanceDanceCard {
 
         atb(new EasyXCostAction(this, (effect, params) -> {
             for (int i = 0; i < effect; i++) {
-                dmgTop(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
+                this.addToBot(new SFXAction("ATTACK_HEAVY"));
+                this.addToBot(new VFXAction(p, new CleaveEffect(), 0.0F));
+                this.addToBot(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_VERTICAL));
             }
             if (effect <= 0) {
                 this.addToBot(new ChangeStanceAction(new AncientStance()));
