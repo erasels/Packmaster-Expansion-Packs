a1b825bf3ef3abf22b291eec9f5d450183300270
modargo <modargo@gmail.com>
2024-03-22 08:43:17 -0400

    Grand Opening pack: fix display issues with grayed out text that uses custom dynamic variables

--- src/main/java/thePackmaster/cards/grandopeningpack/AbstractGrandOpeningCard.java
+++ src/main/java/thePackmaster/cards/grandopeningpack/AbstractGrandOpeningCard.java
@@ -1,6 +1,7 @@
 package thePackmaster.cards.grandopeningpack;
 
 import thePackmaster.cards.AbstractPackmasterCard;
+import thePackmaster.packs.EvenOddPack;
 
 public abstract class AbstractGrandOpeningCard extends AbstractPackmasterCard
 {
@@ -11,5 +12,11 @@ public abstract class AbstractGrandOpeningCard extends AbstractPackmasterCard
     }
     @Override
     public void upp() {
+    }
+
+    public static String makeCardTextGray(String input) {
+        // This skips the replacement of "anniv5:" with "" that the AbstractEvenOddCard version does to avoid issues
+        // with text formatting when using custom dynamic variables (e.g. !anniv5:m2!)
+        return input.replaceAll("(\\s)((?!!|\\[E]|NL))", " " + EvenOddPack.GRAY + "$2");
     }
 }
