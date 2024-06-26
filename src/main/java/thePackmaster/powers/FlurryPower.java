1cb70d0d6e9022fc8da107172f94d5b49d30803e
Alchyr <isithalchyr@gmail.com>
2024-06-26 04:51:46 -0700

    change FlurryPower to onUseCard

--- src/main/java/thePackmaster/powers/FlurryPower.java
+++ src/main/java/thePackmaster/powers/FlurryPower.java
@@ -1,11 +1,10 @@
 package thePackmaster.powers;
 
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.FlashPowerEffect;
-import thePackmaster.powers.AbstractPackmasterPower;
 import thePackmaster.util.Wiz;
 
 public abstract class FlurryPower extends AbstractPackmasterPower {
@@ -13,10 +12,11 @@ public abstract class FlurryPower extends AbstractPackmasterPower {
         super(ID, NAME, PowerType.BUFF, false, owner, amount);
     }
 
-    public abstract void onFlurry(AbstractCard played, AbstractMonster m);
+    public abstract void onFlurry(AbstractCard played, AbstractCreature c);
 
-    public void onPlayCard(AbstractCard card, AbstractMonster m) {
-        onFlurry(card, m);
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        onFlurry(card, action.target);
         AbstractDungeon.effectList.add(new FlashPowerEffect(this));
     }
 
