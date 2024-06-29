db6f407e65b4f710113eb308c090fb1bd6dd339a
Cláuvin <Clauvin@users.noreply.github.com>
2024-06-28 22:40:18 -0300

    Updated Forcefield.java with the new version

--- src/main/java/thePackmaster/cards/royaltypack/Forcefield.java
+++ src/main/java/thePackmaster/cards/royaltypack/Forcefield.java
@@ -1,8 +1,10 @@
 package thePackmaster.cards.royaltypack;
 
 import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.defect.ReinforcedBodyAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.actions.royaltypack.ForcefieldAction;
 import thePackmaster.actions.royaltypack.TributeOrAusterityAction;
 import thePackmaster.cards.royaltypack.optioncards.ForcefieldAusterity;
 import thePackmaster.cards.royaltypack.optioncards.ForcefieldTribute;
@@ -13,28 +15,22 @@ import static thePackmaster.SpireAnniversary5Mod.makeID;
 public class Forcefield extends AbstractRoyaltyCard {
 
     public final static String ID = makeID("Forcefield");
-    public final static int TRIBUTE_MAXHP = 15;
-    public final static int AUSTERITY_MAXHP = 8;
 
     public Forcefield() {
-        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
-        baseMagicNumber = magicNumber = AUSTERITY_MAXHP;
-        baseSecondMagic = secondMagic = TRIBUTE_MAXHP;
+        super(ID, -1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseBlock = block = 8;
+        baseSecondMagic = secondMagic = 0;
     }
 
     @Override
     public void upp() {
-        this.upgradeMagicNumber(3);
-        this.upgradeSecondMagic(3);
+        this.upgradeSecondMagic(1);
     }
 
     @Override
     public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
-        Wiz.atb(new DiscardAction(abstractPlayer, abstractPlayer, 1, true));
-
-        AbstractRoyaltyCard forcefieldTributeChoiceCard = new ForcefieldTribute(secondMagic);
-        AbstractRoyaltyCard forcefieldAusterityChoiceCard = new ForcefieldAusterity(magicNumber);
-
-        Wiz.atb(new TributeOrAusterityAction(forcefieldTributeChoiceCard, forcefieldAusterityChoiceCard));
+        this.addToBot(new ForcefieldAction(
+                abstractPlayer, this.block,
+                this.freeToPlayOnce, this.energyOnUse, this.secondMagic));
     }
 }
