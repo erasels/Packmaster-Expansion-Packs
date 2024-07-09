ab3423f39f744187566e95a3ed2d6f450fcb3a38
Dj0z <dj0z@live.fr>
2024-07-09 06:22:34 +0200

    Finished the pack's card list and summary.

---
+++ src/main/java/thePackmaster/packs/SiegePack.java
@@ -0,0 +1,37 @@
+package thePackmaster.packs;
+
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import thePackmaster.SpireAnniversary5Mod;
+import thePackmaster.cards.siegepack.*;
+
+import java.util.ArrayList;
+
+public class SiegePack extends AbstractCardPack {
+    public static final String ID = SpireAnniversary5Mod.makeID("SiegePack");
+    private static final UIStrings UI_STRINGS = CardCrawlGame.languagePack.getUIString(ID);
+    public static final String NAME = UI_STRINGS.TEXT[0];
+    public static final String DESC = UI_STRINGS.TEXT[1];
+    public static final String AUTHOR = UI_STRINGS.TEXT[2];
+
+    public SiegePack() {
+        super(ID, NAME, DESC, AUTHOR, new PackSummary(4, 4, 2, 2, 3, PackSummary.Tags.Attacks, PackSummary.Tags.Debuffs, PackSummary.Tags.Powers));
+//        hatHidesHair = true;
+    }
+
+    @Override
+    public ArrayList<String> getCards() {
+        ArrayList<String> cards = new ArrayList<>();
+        cards.add(Shelling.ID);
+        cards.add(Suppress.ID);
+        cards.add(BallisticStrike.ID);
+        cards.add(DefensePlanning.ID);
+        cards.add(ThinkTwice.ID);
+        cards.add(Logistics.ID);
+        cards.add(ShellForge.ID);
+        cards.add(Shrapnel.ID);
+        cards.add(DigIn.ID);
+        cards.add(Prevail.ID);
+        return cards;
+    }
+}
