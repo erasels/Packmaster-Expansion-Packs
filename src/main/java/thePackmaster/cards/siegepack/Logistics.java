4430af86c3c67b2b16dc87c75f0a264ce0af4d29
Dj0z <dj0z@live.fr>
2024-07-12 04:51:42 +0200

    Reworked Logistics to reduce common debuffs from self instead of applying Weak to enemy, which fits better thematically.

--- src/main/java/thePackmaster/cards/siegepack/Logistics.java
+++ src/main/java/thePackmaster/cards/siegepack/Logistics.java
@@ -2,28 +2,37 @@ package thePackmaster.cards.siegepack;
 
 import com.evacipated.cardcrawl.mod.stslib.patches.FlavorText;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.FrailPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 import com.megacrit.cardcrawl.powers.WeakPower;
 import thePackmaster.powers.siegepack.ShellPower;
 import thePackmaster.util.Wiz;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.cards.siegepack.FlavorConstants.*;
+import static thePackmaster.util.Wiz.atb;
 
 public class Logistics extends AbstractSiegeCard {
     public final static String ID = makeID("Logistics");
     private static final int COST = 1;
-    private static final int WEAK_AMOUNT = 2;
+
+    //private static final int WEAK_AMOUNT = 2;
     //private static final int UPGRADE_WEAK_AMOUNT = 1;
+    private static final int DEBUFF_REMOVAL_PLUS_DRAW = 1;
+    private static final int UPGRADE_DEBUFF_REMOVAL_PLUS_DRAW = 1;
+
     private static final int SHELL_GAIN = 1;
-    private static final int CARD_DRAW = 2;
-    private static final int UPGRADE_CARD_DRAW = 1;
+
+    private static final int CARD_DRAW = 1;
+    //private static final int UPGRADE_CARD_DRAW = 1;
 
     public Logistics() {
-        super(ID, COST, CardType.SKILL, CardRarity.COMMON, CardTarget.ENEMY);
-        baseMagicNumber = magicNumber = WEAK_AMOUNT;
-        baseSecondMagic = secondMagic = CARD_DRAW;
+        super(ID, COST, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
+        baseMagicNumber = magicNumber = DEBUFF_REMOVAL_PLUS_DRAW;
+        baseSecondMagic = secondMagic = magicNumber + CARD_DRAW;
 
         FlavorText.AbstractCardFlavorFields.flavorBoxType.set(this, FLAVOR_BOX_TYPE);
         FlavorText.AbstractCardFlavorFields.boxColor.set(this, FLAVOR_BOX_COLOR);
@@ -31,16 +40,22 @@ public class Logistics extends AbstractSiegeCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.applyToEnemy(m, new WeakPower(m, magicNumber, false));
+        //Wiz.applyToEnemy(m, new WeakPower(m, magicNumber, false));
+        ReduceCommonDebuffs(p);
+
         Wiz.applyToSelf(new ShellPower(p, SHELL_GAIN));
         addToBot(new DrawCardAction(secondMagic));
     }
 
-    @Override
-    public void upp() {
-        //upgradeMagicNumber(UPGRADE_WEAK_AMOUNT);
-        upgradeSecondMagic(UPGRADE_CARD_DRAW);
+    private void ReduceCommonDebuffs(AbstractPlayer p) {
+        atb(new ReducePowerAction(p, p, FrailPower.POWER_ID, this.magicNumber));
+        atb(new ReducePowerAction(p, p, VulnerablePower.POWER_ID, this.magicNumber));
+        atb(new ReducePowerAction(p, p, WeakPower.POWER_ID, this.magicNumber));
     }
 
-
+    @Override
+    public void upp() {
+        upgradeMagicNumber(UPGRADE_DEBUFF_REMOVAL_PLUS_DRAW);
+        upgradeSecondMagic(UPGRADE_DEBUFF_REMOVAL_PLUS_DRAW);
+    }
 }
