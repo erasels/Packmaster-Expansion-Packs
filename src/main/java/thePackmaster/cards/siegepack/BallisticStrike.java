640e7c534a7eaa08bdc7426b7a8449baed6bedc2
Dj0z <dj0z@live.fr>
2024-07-13 01:25:52 +0200

    BallisticStrike new rework with better single-target damage and much nicer animation, but worse group targeting (that could be fixed in the future though).

--- src/main/java/thePackmaster/cards/siegepack/BallisticStrike.java
+++ src/main/java/thePackmaster/cards/siegepack/BallisticStrike.java
@@ -1,40 +1,43 @@
 package thePackmaster.cards.siegepack;
 
-import basemod.cardmods.ExhaustMod;
-import basemod.cardmods.RetainMod;
-import basemod.helpers.CardModifierManager;
 import com.evacipated.cardcrawl.mod.stslib.patches.FlavorText;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.DarkSmokePuffEffect;
 import com.megacrit.cardcrawl.vfx.combat.ExplosionSmallEffect;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import thePackmaster.actions.siegepack.BallisticStrikeAction;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.cards.siegepack.FlavorConstants.*;
+import static thePackmaster.util.Wiz.atb;
 
 //The single-target damage is stored in SecondDamage because "DmgAllEnemies" methods hardcodedly use Damage.
 public class BallisticStrike extends AbstractSiegeCard {
     public final static String ID = makeID("BallisticStrike");
     private static final int COST = 3;
-    private static final int TARGETED_DAMAGE = 11;
-    private static final int UPGRADE_TARGETED_DAMAGE = 4;
+    private static final int TARGETED_DAMAGE = 10;
+    //private static final int UPGRADE_TARGETED_DAMAGE = 4;
+
+    private static final int HITS = 2;
+    private static final int UPGRADE_HITS = 1;
+
     /*private static final int DAMAGE_OTHERS = 6;
     private static final int UPGRADE_DAMAGE_OTHERS = 5;*/
-    private static final int TEMP_SHELLINGS = 1;
-    //private static final int UPGRADE_TEMP_SHELLINGS = 1;
+
+    /*private static final int TEMP_SHELLINGS = 1;
+    private static final int UPGRADE_TEMP_SHELLINGS = 1;*/
 
     public BallisticStrike() {
-        super(ID, COST, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
-        //baseDamage = DAMAGE_OTHERS;
-        baseSecondDamage = TARGETED_DAMAGE;
-        baseMagicNumber = magicNumber = TEMP_SHELLINGS;
-        //isMultiDamage = true;
+        super(ID, COST, CardType.ATTACK, CardRarity.RARE, CardTarget.ALL_ENEMY);
+        baseDamage = TARGETED_DAMAGE;
+//        baseSecondDamage = TARGETED_DAMAGE;
+        baseMagicNumber = magicNumber = HITS;
+        isMultiDamage = true;
         tags.add(CardTags.STRIKE);
-        cardsToPreview = new Shelling();
+//        cardsToPreview = new Shelling();
 
         FlavorText.AbstractCardFlavorFields.flavorBoxType.set(this, FLAVOR_BOX_TYPE);
         FlavorText.AbstractCardFlavorFields.boxColor.set(this, FLAVOR_BOX_COLOR);
@@ -42,30 +45,35 @@ public class BallisticStrike extends AbstractSiegeCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        //Deal damage to target, double value if it's attacking. Then damage all Other enemies.
-        this.addToBot(new VFXAction(new ExplosionSmallEffect(m.hb.cX, m.hb.cY), 0.35F));
+        //Queue actions to damage random enemy X times, double damage if attacking.
+        for (int i = 0; i < this.magicNumber; ++i) {
+            /*if (p.hasPower(ShellPower.POWER_ID)) {
+                this.addToTop(new VFXAction(new ExplosionSmallEffect(p.hb.cX, p.hb.cY), 0.04F));
+            }*/
+            this.addToTop(new VFXAction(new ExplosionSmallEffect(p.hb.cX, p.hb.cY), 0.05F));
+            this.addToTop(new VFXAction(new DarkSmokePuffEffect(p.hb.cX, p.hb.cY), 0.10F));
+            atb(new BallisticStrikeAction(p, this));
+        }
 
-        if (isRetaliatory(m)) {
+        /*if (isRetaliatory(m)) {
             //Wiz.doDmg(m, secondDamage, DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
             altDmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
         } else {
             altDmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-        }
+        }*/
 
         //Add temp Shellings
-        AbstractCard tempShelling = new Shelling();
+        /*AbstractCard tempShelling = new Shelling();
         CardModifierManager.addModifier(tempShelling, new ExhaustMod());
         CardModifierManager.addModifier(tempShelling, new RetainMod());
         tempShelling.cost = 0;  //For some reason this waits until end of turn.
-        tempShelling.costForTurn = 0;
+        tempShelling.costForTurn = 0;*/
+
         //Fine-tune this and amount of Shellings for balance.
-        if (upgraded) {
+        /*if (upgraded) {
             tempShelling.upgrade();
         }
-        this.addToBot(new MakeTempCardInHandAction(tempShelling, magicNumber));
-
-        //Auto-play the Shellings whenever possible. Not sure how the below works.
-        //AbstractDungeon.actionManager.addCardQueueItem(Shelling);
+        this.addToBot(new MakeTempCardInHandAction(tempShelling, magicNumber));*/
 
         //Does nothing.
         /*for (AbstractMonster mo : getEnemies()) {
@@ -85,7 +93,7 @@ public class BallisticStrike extends AbstractSiegeCard {
     }
 
     //calculateCardDamage is Auto-called before every card is played.
-    @Override
+    /*@Override
     public void calculateCardDamage(AbstractMonster mo){
         calculateTrueDamage(mo, true);
     }
@@ -99,14 +107,25 @@ public class BallisticStrike extends AbstractSiegeCard {
         }
         this.rawDescription = cardStrings.DESCRIPTION;
         this.initializeDescription();
-    }
-
-    public boolean isRetaliatory(AbstractMonster m) {return m != null && m.getIntentBaseDmg() >= 0;}
+    }*/
 
     public void triggerOnGlowCheck() {
         this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        //Light up in a stronger color if ALL enemies are attacking. Modargo said no.
+        /*ArrayList<AbstractMonster> enemies = AbstractDungeon.getCurrRoom().monsters.monsters;
+        int attackers = 0;
         for (AbstractMonster m : AbstractDungeon.getCurrRoom().monsters.monsters) {
             if (!m.isDeadOrEscaped() && m.getIntentBaseDmg() >= 0) {
+                attackers += 1;
+                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+                break;
+            }
+        }
+        if (attackers == enemies.size()) {this.glowColor = AbstractCard.GREEN_BORDER_GLOW_COLOR.cpy();}
+        return;*/
+        for (AbstractMonster m : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            //if (!m.isDeadOrEscaped() && m.getIntentBaseDmg() >= 0) {
+            if (isRetaliatory(m)) {
                 this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
                 break;
             }
@@ -116,8 +135,12 @@ public class BallisticStrike extends AbstractSiegeCard {
     @Override
     public void upp() {
         //upgradeDamage(UPGRADE_DAMAGE_OTHERS);
-        upgradeSecondDamage(UPGRADE_TARGETED_DAMAGE);
+        //upgradeSecondDamage(UPGRADE_TARGETED_DAMAGE);
+        upgradeMagicNumber(UPGRADE_HITS);
     }
+
+    public boolean isRetaliatory(AbstractMonster m)
+    {return m != null && !m.isDeadOrEscaped() && m.getIntentBaseDmg() >= 0;}
 }
 /*
 (Recorded elsewhere : much Discord channel code.)
@@ -134,7 +157,7 @@ public void use(AbstractPlayer p, AbstractMonster m) {
             applyToEnemy(mo, new WeakPower(mo, secondMagic, false));
 }
 
-//What ENBEON would do:
+//What Enbeon would do:
 @Override
 public void calculateCardDamage(AbstractMonster mo) {
     calculateTrueDamage(mo, true);
@@ -157,7 +180,7 @@ public void use(AbstractPlayer p, AbstractMonster m) {
 
 //================ MAKE AND AUTOPLAY CARDS ================
 
-PERFECT REF : Create a card and autoplay it immediately at random. Daggerstorm (dimensiongatepack2).
+PERFECT REF : Create a card and autoplay it immediately at a random enemy. Daggerstorm (dimensiongatepack2).
 
 public void use(AbstractPlayer p, AbstractMonster m) {
     applyToSelf(new DaggerstormPower(p, 1));
