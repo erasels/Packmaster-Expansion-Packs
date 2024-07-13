fd08169776c01398c1af0c321df461ab562b1928
Cláuvin <Clauvin@users.noreply.github.com>
2024-07-13 15:41:07 -0300

    Mining Strike done

--- src/main/java/thePackmaster/cards/royaltypack/MiningStrike.java
+++ src/main/java/thePackmaster/cards/royaltypack/MiningStrike.java
@@ -1,8 +1,12 @@
 package thePackmaster.cards.royaltypack;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.GainGoldAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.GainGoldTextEffect;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -13,8 +17,10 @@ public class MiningStrike extends AbstractRoyaltyCard {
 
     public MiningStrike(){
         super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        this.tags.add(CardTags.STRIKE);
         baseDamage = damage = 15;
         baseMagicNumber = magicNumber = 15;
+        this.exhaust = true;
     }
 
     @Override
@@ -26,5 +32,8 @@ public class MiningStrike extends AbstractRoyaltyCard {
     @Override
     public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
         dmg(abstractMonster, AbstractGameAction.AttackEffect.SLASH_HEAVY);
+        this.addToBot(new GainGoldAction(magicNumber));
+        AbstractDungeon.effectList.add(new GainGoldTextEffect(magicNumber));
+        CardCrawlGame.sound.play("GOLD_GAIN", 0.1F);
     }
 }
