81b5e2c07f2d4fef03e3a640bf34a8d9c98683b5
Cláuvin <Clauvin@users.noreply.github.com>
2024-10-12 17:57:38 -0300

    Trying to understand why the locator is not working as it should -_-

---
+++ src/main/java/thePackmaster/patches/royaltypack/OnPotionUsePatch.java
@@ -0,0 +1,40 @@
+package thePackmaster.patches.royaltypack;
+
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.audio.SoundMaster;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.ui.panels.PotionPopUp;
+import com.megacrit.cardcrawl.ui.panels.TopPanel;
+import javassist.CtBehavior;
+import thePackmaster.powers.royaltypack.OnPotionUsePower;
+
+@SpirePatch(
+        clz = PotionPopUp.class,
+        method = "updateInput"
+)
+public class OnPotionUsePatch {
+
+    @SpireInsertPatch(
+        locator=Locator.class
+    )
+
+    public static void onPotionUse(PotionPopUp __instance) {
+        for (AbstractPower power : AbstractDungeon.player.powers) {
+            System.out.println(power.name);
+            if (power instanceof OnPotionUsePower) {
+                ((OnPotionUsePower) power).onPotionUse();
+            }
+        }
+        System.out.println("HM.");
+    }
+
+    private static class Locator extends SpireInsertLocator {
+        @Override
+        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+            Matcher finalMatcher = new Matcher.MethodCallMatcher(SoundMaster.class, "play");
+            int[] matches = LineFinder.findAllInOrder(ctMethodToPatch, finalMatcher);
+            return new int[]{matches[1]+1};
+        }
+    }
+}
