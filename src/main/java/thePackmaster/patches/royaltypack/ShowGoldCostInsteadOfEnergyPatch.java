36aa678129d0c314716df737dc553c3d5b45afbd
Cláuvin <Clauvin@users.noreply.github.com>
2024-11-03 10:03:44 -0300

    Why it doesn't work?

--- src/main/java/thePackmaster/patches/royaltypack/ShowGoldCostInsteadOfEnergyPatch.java
+++ src/main/java/thePackmaster/patches/royaltypack/ShowGoldCostInsteadOfEnergyPatch.java
@@ -1,8 +1,12 @@
 package thePackmaster.patches.royaltypack;
 
+import basemod.ReflectionHacks;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.vfx.FastCardObtainEffect;
 import javassist.CtBehavior;
 
 @SpirePatch(
@@ -17,7 +21,20 @@ public class ShowGoldCostInsteadOfEnergyPatch {
     )
 
     public static void ShowGoldCostInsteadOfEnergy(AbstractCard __instance){
-        System.out.println("Foi");
+        if (AbstractDungeon.player != null && AbstractDungeon.player.hand.contains(__instance) &&
+                AbstractDungeon.player.hasPower("HiredSupportPower")) {
+            Color costColor = (Color) ReflectionHacks.getPrivate(__instance, Color.class, "costColor");
+            String text = (String) ReflectionHacks.getPrivate(__instance, String.class, "text");
+            costColor = Color.YELLOW;
+            if (__instance.cost == -1){
+                text = "10 X";
+            } else {
+                text = Integer.toString(__instance.costForTurn*10);
+            }
+            ReflectionHacks.setPrivate(__instance, Color.class, "costColor", costColor);
+            ReflectionHacks.setPrivate(__instance, String.class, "text", text);
+            System.out.println("Huh");
+        }
     }
 
 
