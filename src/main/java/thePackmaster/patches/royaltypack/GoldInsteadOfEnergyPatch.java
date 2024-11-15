438aa10e424408954ad7ce27a990e2c334b6711b
Cláuvin <Clauvin@users.noreply.github.com>
2024-11-14 22:32:11 -0300

    Fixed Gold use not working with X cards

--- src/main/java/thePackmaster/patches/royaltypack/GoldInsteadOfEnergyPatch.java
+++ src/main/java/thePackmaster/patches/royaltypack/GoldInsteadOfEnergyPatch.java
@@ -5,7 +5,6 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
-import thePackmaster.powers.overwhelmingpack.FreeSkillPower;
 import thePackmaster.powers.royaltypack.HiredSupportPower;
 
 @SpirePatch(
@@ -21,7 +20,8 @@ public class GoldInsteadOfEnergyPatch {
             return AbstractDungeon.player != null && AbstractDungeon.currMapNode != null &&
                     AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT &&
                     AbstractDungeon.player.hasPower(HiredSupportPower.POWER_ID) &&
-                    __instance.costForTurn * 10 <= AbstractDungeon.player.gold;
+                    __instance.costForTurn * 10 <= AbstractDungeon.player.gold &&
+                    (__instance.cost > 0 || __instance.cost == -1);
         }
         return true;
     }
