9428686713eb95fd9c34f2fa40c982d0f56dd73a
Cláuvin <Clauvin@users.noreply.github.com>
2024-11-30 17:58:34 -0300

    RetainerStrike now is better, UI-wise

--- src/main/java/thePackmaster/cards/royaltypack/RetainerStrike.java
+++ src/main/java/thePackmaster/cards/royaltypack/RetainerStrike.java
@@ -1,5 +1,7 @@
 package thePackmaster.cards.royaltypack;
 
+import basemod.helpers.CardModifierManager;
+import com.badlogic.gdx.graphics.Color;
 import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsAction;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -9,8 +11,10 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePackmaster.SpireAnniversary5Mod;
+import thePackmaster.cardmodifiers.royaltypack.RetainForOneTurnModifier;
 import thePackmaster.util.Wiz;
 
+import java.awt.*;
 import java.util.List;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
@@ -49,8 +53,9 @@ public class RetainerStrike extends AbstractRoyaltyCard {
             else {
                 toRetain = hand.getRandomCard(AbstractDungeon.cardRandomRng);
             }
-            toRetain.retain = true;
-            toRetain.superFlash();
+            CardModifierManager.addModifier(toRetain, new RetainForOneTurnModifier());
+            toRetain.flash();
+
         }
         else {
             if (AbstractDungeon.player.hand.isEmpty()) {
@@ -66,13 +71,12 @@ public class RetainerStrike extends AbstractRoyaltyCard {
                 }
                 else if (handWithoutRetain.size() == 1){
                     AbstractCard card = hand.getRandomCard(AbstractDungeon.cardRandomRng);
-                    card.retain = true;
-                    card.flash();
+                    CardModifierManager.addModifier(card, new RetainForOneTurnModifier());
                 } else {
                     Wiz.atb(new SelectCardsAction(handWithoutRetain.group, CHOOSE_RETAIN_TEXT[0],
                             (List<AbstractCard> cards) -> {
                                 AbstractCard card = cards.get(0);
-                                card.retain = true;
+                                CardModifierManager.addModifier(card, new RetainForOneTurnModifier());
                                 card.flash();
                             }
                     ));
