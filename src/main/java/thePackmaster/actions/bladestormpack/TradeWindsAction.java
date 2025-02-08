162bfa930479bc7745827874a7078fba18b73f48
Dj0z <dj0z@live.fr>
2025-02-08 20:20:16 +0100

    Added TradeWinds SFX & VFX for when a rare card is chosen, since the healing outcome had those.

--- src/main/java/thePackmaster/actions/bladestormpack/TradeWindsAction.java
+++ src/main/java/thePackmaster/actions/bladestormpack/TradeWindsAction.java
@@ -4,16 +4,23 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.vfx.GainPennyEffect;
+
+import static com.megacrit.cardcrawl.dungeons.AbstractDungeon.player;
 
 /*REFS: AttackVial (womaninbluepack), StrikingStrike (strikepack), Mimicry & Paintbrush (creativitypack),
 RazorWind (dragonwrathpack), DualHeal (summonerspellspack), RandomUpgradeWithVfxAction (upgradespack),
-NeowReward (base game)*/
+NeowReward & DamageAction (base game), BallisticStrike (siegepack)*/
 public class TradeWindsAction extends AbstractGameAction {
     private final AbstractCreature p;
     private final AbstractCard chosenCard;
     private final int healIfCommon;
     private final int goldLossIfRare;
+    private static final float VFX_X_OFFSET = -1080F * Settings.scale;
+    private static final float VFX_Y_OFFSET = 360F * Settings.scale;
 
     public TradeWindsAction(AbstractCreature source, int heal, int goldLoss, AbstractCard chosenCard) {
         this.p = source;
@@ -28,12 +35,34 @@ public class TradeWindsAction extends AbstractGameAction {
         //Depending on rarity of the card printed by the Discover calling this Action, heal or lose gold.
         if (chosenCard.rarity == AbstractCard.CardRarity.COMMON) {
             addToTop(new HealAction(p, p, healIfCommon, 0));
+
         } else if (chosenCard.rarity == AbstractCard.CardRarity.RARE) {
             addToTop(new AbstractGameAction() {
+                //Lose gold with SFX and VFX to make player aware in case they didn't realize.
+                //Based on DamageAction (base game).
                 @Override
                 public void update() {
                     isDone = true;
-                    AbstractDungeon.player.loseGold(TradeWindsAction.this.goldLossIfRare);  //from NeowReward (base game).
+                    if (player.gold == 0) {
+                        return;
+                    }
+
+                    //Calculate coins for VFX.
+                    int tempGoldLoss = goldLossIfRare;
+                    if (player.gold < tempGoldLoss) {
+                        tempGoldLoss = player.gold;
+                    }
+                    //Remove the actual amount. If player's broke, they "borrow from an ally thanks to the wind".
+                    player.gold -= goldLossIfRare;
+
+                    //SFX & VFX. The wind visually taking away "borrowed" gold would not make sense.
+                    CardCrawlGame.sound.play("GOLD_JINGLE");
+                    for (int i = 0; i < tempGoldLoss; i++) {
+                        AbstractDungeon.effectList.add(new GainPennyEffect(
+                                player, player.hb.cX, player.hb.cY,
+                                player.hb.cX + VFX_X_OFFSET, player.hb.cY + VFX_Y_OFFSET, false
+                        ));
+                    }
                 }
             });
         }
