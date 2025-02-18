ec62e54d56657c22faf77f476a14bc76427380f3
LyrenScalari <74549054+LyrenScalari@users.noreply.github.com>
2025-02-18 10:21:20 -0600

    Hornet Within Rework
    Hornet Within
    Removed orb slot gain
    Cost 2 -> 1
    deal 12 damage -> Deal 3(5) damage for each skill in your hand
    
    Corrected a minor issue with Wicked Weaves upgrade card text

--- src/main/java/thePackmaster/cards/WitchesStrike/HornetWithin.java
+++ src/main/java/thePackmaster/cards/WitchesStrike/HornetWithin.java
@@ -2,11 +2,17 @@ package thePackmaster.cards.WitchesStrike;
 
 import com.megacrit.cardcrawl.actions.defect.ChannelAction;
 import com.megacrit.cardcrawl.actions.defect.IncreaseMaxOrbAction;
+import com.megacrit.cardcrawl.actions.unique.FlechetteAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.green.Bane;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePackmaster.orbs.WitchesStrike.Arcane;
 import thePackmaster.util.Wiz;
+
+import java.util.Iterator;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -15,15 +21,44 @@ public class HornetWithin extends AbstractWitchStrikeCard {
     // intellij stuff attack, enemy, basic, 6, 3,  , , ,
 
     public HornetWithin() {
-        super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
-        damage = baseDamage = 12;
+        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        damage = baseDamage = 3;
         magicNumber = baseMagicNumber = 2;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.doDmg(m,damage);
-        Wiz.atb(new IncreaseMaxOrbAction(1));
+        Wiz.atb(new FlechetteAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL)));
         Wiz.atb(new ChannelAction(new Arcane()));
+        this.rawDescription = cardStrings.DESCRIPTION;// 34
+        this.initializeDescription();// 35
+    }// 36
+
+    public void applyPowers() {
+        super.applyPowers();// 40
+        int count = 0;// 42
+        Iterator var2 = AbstractDungeon.player.hand.group.iterator();// 43
+
+        while(var2.hasNext()) {
+            AbstractCard c = (AbstractCard)var2.next();
+            if (c.type == CardType.SKILL) {// 44
+                ++count;// 45
+            }
+        }
+
+        this.rawDescription = cardStrings.DESCRIPTION;// 49
+        this.rawDescription = this.rawDescription + cardStrings.EXTENDED_DESCRIPTION[0] + count;// 50
+        if (count == 1) {// 51
+            this.rawDescription = this.rawDescription + cardStrings.EXTENDED_DESCRIPTION[1];// 52
+        } else {
+            this.rawDescription = this.rawDescription + cardStrings.EXTENDED_DESCRIPTION[2];// 54
+        }
+
+        this.initializeDescription();// 56
+    }// 57
+
+    public void onMoveToDiscard() {
+        this.rawDescription = cardStrings.DESCRIPTION;// 61
+        this.initializeDescription();// 62
     }
     public void upp() {
         upgradeDamage(2);
