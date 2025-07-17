3b2660344a7607891f623e682b844f7b5a3f3148
modargo <modargo@gmail.com>
2025-07-17 13:01:44 -0400

    Turmoil pack: fix Burnt Offering not discarding the cards

--- src/main/java/thePackmaster/cards/turmoilpack/BurntOffering.java
+++ src/main/java/thePackmaster/cards/turmoilpack/BurntOffering.java
@@ -3,12 +3,21 @@ package thePackmaster.cards.turmoilpack;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePackmaster.SpireAnniversary5Mod;
 import thePackmaster.actions.turmoilpack.AbandonAction;
 import thePackmaster.powers.shamanpack.IgnitePower;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BurntOffering extends AbstractTurmoilCard {
     public static final String ID = SpireAnniversary5Mod.makeID("BurntOffering");
@@ -35,6 +44,14 @@ public class BurntOffering extends AbstractTurmoilCard {
         this.addToBot(new AbandonAction(c -> c.color == CardColor.COLORLESS || c.color == CardColor.CURSE || c.type == CardType.CURSE, l -> {
             this.addToTop(new ApplyPowerAction(m, p, new IgnitePower(m, this.magicNumber * l.size())));
             this.addToTop(new DamageAction(m, new DamageInfo(p, this.damage * l.size(), this.damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+            Collections.reverse(l);
+            for (AbstractCard c : l) {
+                List<CardGroup> piles = Arrays.asList(p.discardPile, p.drawPile, p.hand);
+                piles = piles.stream().filter(pile -> pile.contains(c)).collect(Collectors.toList());
+                if (!piles.isEmpty()) {
+                    this.addToTop(new ExhaustSpecificCardAction(c, piles.get(0)));
+                }
+            }
         }));
     }
 }
