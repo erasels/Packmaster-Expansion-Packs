c84b63cda14d76b2fad835da621b10cf5a707ddc
LyrenScalari <74549054+LyrenScalari@users.noreply.github.com>
2025-08-24 19:39:50 -0500

    Adjust Crystal Moolight

--- src/main/java/thePackmaster/cards/WitchesStrike/CrystalMoonlight.java
+++ src/main/java/thePackmaster/cards/WitchesStrike/CrystalMoonlight.java
@@ -17,7 +17,7 @@ public class CrystalMoonlight extends AbstractWitchStrikeCard {
 
     public CrystalMoonlight() {
         super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
-        magicNumber = baseMagicNumber = 3;
+        magicNumber = baseMagicNumber = 2;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
@@ -27,7 +27,7 @@ public class CrystalMoonlight extends AbstractWitchStrikeCard {
     }
 
     public void upp() {
-        upgradeMagicNumber(1);
+        upgradeMagicNumber(2);
     }
     @Override
     public String cardArtCopy() {
