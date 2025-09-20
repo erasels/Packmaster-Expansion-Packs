4fc1422b0690dee8a987fc01e59c532ea3954de9
Dj0z <dj0z@live.fr>
2025-09-20 07:46:24 +0200

    ThinkTwice's displayed Block now takes current Vigor into account.

--- src/main/java/thePackmaster/cards/siegepack/ThinkTwice.java
+++ src/main/java/thePackmaster/cards/siegepack/ThinkTwice.java
@@ -1,8 +1,8 @@
 package thePackmaster.cards.siegepack;
 
 import com.evacipated.cardcrawl.mod.stslib.patches.FlavorText;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.watcher.VigorPower;
@@ -10,9 +10,9 @@ import thePackmaster.util.Wiz;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.cards.siegepack.FlavorConstants.*;
-import static thePackmaster.util.Wiz.atb;
 
-//REFS : PlotArmor (grandopeningpack), Subdue (bitingcoldpack)
+//REFS : PlotArmor (grandopeningpack), Subdue (bitingcoldpack),
+// HuntingInstincts (monsterhunterpack), BlockVial (womaninbluepack).
 public class ThinkTwice extends AbstractSiegeCard {
     public final static String ID = makeID("ThinkTwice");
     private static final int COST = 2;
@@ -32,15 +32,25 @@ public class ThinkTwice extends AbstractSiegeCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
+
         AbstractPower currentVigor = p.getPower(VigorPower.POWER_ID);
-        if (currentVigor != null && currentVigor.amount > 0) {
-            atb(new GainBlockAction(p, block + currentVigor.amount));
+        if (currentVigor != null) {
             Wiz.removePower(currentVigor);
-        } else {
-            blck();
         }
 
         Wiz.applyToSelf(new VigorPower(p, magicNumber));
+    }
+
+    @Override
+    public void applyPowersToBlock() {      //Updates the block displayed on the card while in hand, hovered or not.
+        super.applyPowersToBlock();
+
+        AbstractPower previewVigor = AbstractDungeon.player.getPower(VigorPower.POWER_ID);
+        if (previewVigor != null && previewVigor.amount > 0) {
+            this.block += previewVigor.amount;
+            isBlockModified = true;
+        }
     }
 
     @Override
