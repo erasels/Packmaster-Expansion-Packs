084e2796268e978e4c42e7adb0726efb3a49c7dc
Dj0z <dj0z@live.fr>
2025-09-20 06:35:58 +0200

    Refactored RampingTurnVigorGainPower.

--- src/main/java/thePackmaster/powers/siegepack/RampingTurnVigorGainPower.java
+++ src/main/java/thePackmaster/powers/siegepack/RampingTurnVigorGainPower.java
@@ -3,30 +3,27 @@ package thePackmaster.powers.siegepack;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 import thePackmaster.SpireAnniversary5Mod;
 import thePackmaster.powers.AbstractPackmasterPower;
 import thePackmaster.util.Wiz;
 
-import java.util.Objects;
-
 import static com.megacrit.cardcrawl.dungeons.AbstractDungeon.player;
 
-//REFS: EventHorizon (marisapack)
+//REFS: EventHorizon (marisapack), ControlledBurnPower (fueledpack), PolarityPower (magnetizepack).
 public class RampingTurnVigorGainPower extends AbstractPackmasterPower {
     public static final String POWER_ID = SpireAnniversary5Mod.makeID(RampingTurnVigorGainPower.class.getSimpleName());
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
-    private final int increaseFix;
+    private final int stackAmountRampingPerTurn;
 
-    public RampingTurnVigorGainPower(AbstractCreature owner, int amount, int increase) {
+    public RampingTurnVigorGainPower(AbstractCreature owner, int amount, int amountRampingPerTurn) {
         super(POWER_ID, NAME, PowerType.BUFF, true, owner, amount);
         this.amount = amount;
-        this.amount2 = increase;
-        this.increaseFix = increase;
+        this.amount2 = amountRampingPerTurn;
+        this.stackAmountRampingPerTurn = amountRampingPerTurn;
 
         this.priority = 8000; // EventHorizonPower priority should be 5 by default.
         // This should trigger after it. Could be useful.
@@ -43,10 +40,9 @@ public class RampingTurnVigorGainPower extends AbstractPackmasterPower {
     }
 
     @Override
-    public void onApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
-        if (Objects.equals(power.ID, RampingTurnVigorGainPower.POWER_ID)) {
-            amount2 += increaseFix;
-        }
+    public void stackPower(int stackAmount) {
+        super.stackPower(stackAmount);
+        amount2 += stackAmountRampingPerTurn;
     }
 
     public void updateDescription() {
