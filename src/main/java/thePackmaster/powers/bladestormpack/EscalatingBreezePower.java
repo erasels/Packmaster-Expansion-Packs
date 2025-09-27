8c08fac575bcbf240c83ba084b9c5cd14874ed1c
Dj0z <dj0z@live.fr>
2025-09-27 15:15:33 +0200

    Gale-Force nerfed, Escalating Breeze reworked.

--- src/main/java/thePackmaster/powers/bladestormpack/EscalatingBreezePower.java
+++ src/main/java/thePackmaster/powers/bladestormpack/EscalatingBreezePower.java
@@ -1,29 +1,23 @@
 package thePackmaster.powers.bladestormpack;
 
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import thePackmaster.actions.bladestormpack.EscalatingBreezeAction;
 import thePackmaster.powers.AbstractPackmasterPower;
 
-import java.util.ArrayList;
-
-import static com.megacrit.cardcrawl.dungeons.AbstractDungeon.player;
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
-//REFS: RummageDrawAction (packmaster), RunedMithrilRod (warlockpack), RetainCardsAction (base game)
+//REFS: RetainCardsAction (base game)
 public class EscalatingBreezePower  extends AbstractPackmasterPower {
     public static final String POWER_ID = makeID("EscalatingBreezePower");
     private static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
     private static final String[] DESCRIPTIONS = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
 
-    private ArrayList<AbstractCard> CARDS_TO_REDUCE = null;
-
-    public EscalatingBreezePower(AbstractCreature owner, int amount, int costReduction) {
+    public EscalatingBreezePower(AbstractCreature owner, int amount) {
         super(POWER_ID, NAME, PowerType.BUFF, true, owner, amount);
-        this.amount2 = costReduction;
         updateDescription();
         //If ever needed, set priority to avoid bugs, like "this.priority = -42068".
     }
@@ -31,37 +25,24 @@ public class EscalatingBreezePower  extends AbstractPackmasterPower {
     //Based on RetainCardsAction (base game)
     @Override
     public void atEndOfTurn(boolean isPlayer) {
-        //Choose and retain the Attacks that will have their cost reduced (at the start of next turn).
-        addToBot(new EscalatingBreezeAction(player, amount, this)); //Makes the list of cards.
-    }
+        //Retain all Attacks.
+        if (AbstractDungeon.player.hand.group.isEmpty()) { return; }
 
-    @Override
-    public void atStartOfTurnPostDraw() {
-        //Do the cost reductions on cards previously chosen at end of turn.
-        if (CARDS_TO_REDUCE == null || CARDS_TO_REDUCE.isEmpty()) {     //No cards were chosen.
-            addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this));
-            return;
+        for (AbstractCard c : AbstractDungeon.player.hand.group) {
+            if (c.type == AbstractCard.CardType.ATTACK) {
+                if (!c.isEthereal) {
+                    c.retain = true;
+                }
+                //AbstractDungeon.player.hand.addToTop(c);  //Is this still useful?
+            }
         }
-
         this.flashWithoutSound();
-        for (AbstractCard c : CARDS_TO_REDUCE) {
-            if (c == null || !AbstractDungeon.player.hand.group.contains(c) || c.type != AbstractCard.CardType.ATTACK) {
-                continue;
-            }
 
-            //Based on RunedMithrilRod (warlockpack)
-            if (c.cost >= 0 && c.costForTurn > 0) {
-                c.setCostForTurn(Math.max(0, c.costForTurn - this.amount2));
-                c.isCostModifiedForTurn = true;
-                c.superFlash();
-            }
+        if (amount <= 0) {
+            addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+        } else {
+            addToBot(new ReducePowerAction(this.owner, this.owner, this, 1));
         }
-        AbstractDungeon.player.hand.refreshHandLayout();    //Purpose unknown, recommended by Enbeon.
-        addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this));
-    }
-
-    public void retrieveChosenCards (ArrayList<AbstractCard> cardsArray) {
-        CARDS_TO_REDUCE = cardsArray;
     }
 
     @Override
@@ -72,6 +53,5 @@ public class EscalatingBreezePower  extends AbstractPackmasterPower {
         } else {
             this.description += this.amount + DESCRIPTIONS[2];
         }
-        this.description += DESCRIPTIONS[3];
     }
 }
