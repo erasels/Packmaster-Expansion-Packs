b1e8842378ad7875fe306776c8be2df9d961d529
jgrandizio49-design <jgrandizio49@gmail.com>
2026-08-11 17:16:03 -0500

    Update src/main/java/thePackmaster/cards/cellspack/SwiftSword.java
    Code reduced
    
    Co-authored-by: modargo <62083413+modargo@users.noreply.github.com>

--- src/main/java/thePackmaster/cards/cellspack/SwiftSword.java
+++ src/main/java/thePackmaster/cards/cellspack/SwiftSword.java
@@ -15,7 +15,7 @@ public class SwiftSword extends AbstractCellsCard {
 
     public SwiftSword() {
         super(ID, 0, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = damage = 3;
+        baseDamage = 3;
         baseMagicNumber = magicNumber = 1;
     }
 
