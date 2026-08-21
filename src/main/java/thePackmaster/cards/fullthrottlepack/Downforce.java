cf8171d4c59c4b5c96f6b8ceb681a92856868f22
erasels <emanuelbedburdick@yahoo.de>
2026-08-21 20:44:07 +0200

    fix: Fix broken author list and missing import in Downforce

--- src/main/java/thePackmaster/cards/fullthrottlepack/Downforce.java
+++ src/main/java/thePackmaster/cards/fullthrottlepack/Downforce.java
@@ -1,21 +1,13 @@
 package thePackmaster.cards.fullthrottlepack;
 
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.CardGroup;
+import com.evacipated.cardcrawl.mod.stslib.actions.common.AllEnemyApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.powers.watcher.VigorPower;
-import thePackmaster.actions.fullthrottlepack.ApplyRandomNitroAction;
-import thePackmaster.patches.fullthrottlepack.AddToHandPatches;
 import thePackmaster.powers.shamanpack.IgnitePower;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
-import static thePackmaster.util.Wiz.adp;
 import static thePackmaster.util.Wiz.atb;
 
 
